2e2b6e4cbd6451a0e863f61739b27dfddad37755
alinapevzner <alina.pevzner@aviasales.ru>
2024-07-01 16:07:22 +0100

    test1

--- src/main/java/ru/bot/cons/VarConst.java
+++ src/main/java/ru/bot/cons/VarConst.java
@@ -2,7 +2,7 @@ package ru.bot.cons;
 
 public class VarConst {
     public static final String START = "/start";
-    public static final String START_PLANNING = "Начать планирование";
+    public static final String START_PLANNING = "Начать планирование1";
     public static final String END_PLANNING = "Закончить планирование";
     public static final String SHOW_DEALS = "Показать делишки";
 }
